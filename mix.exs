5fae911ed73072035ae26859bf90896bdd9a72a4
Alan Voss <avoss@rent.com>
2017-10-17 14:21:08 -0500

    adding distillery/conform

--- mix.exs
+++ mix.exs
@@ -18,10 +18,11 @@ defmodule QueueBot.Mixfile do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      {:cowboy, "~> 1.1.2"},
-      {:plug, "~> 1.4.0"},
-      {:poison, "~> 2.0"}
+    [{:conform, "~> 2.1.2", runtime: false},
+     {:cowboy, "~> 1.1.2"},
+     {:distillery, "~> 1.0.0", runtime: false},
+     {:plug, "~> 1.4.0"},
+     {:poison, "~> 2.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
